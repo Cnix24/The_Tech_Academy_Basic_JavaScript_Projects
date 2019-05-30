e6b19e26391d022c5fb69613c6c544ea4ac62a4f
Cnix24 <calvinattl24@gmail.com>
2019-05-30 13:00:39 -0600

    JavaScript_Assignments

---
+++ step_38.js
@@ -0,0 +1,5 @@
+//print all numbers from 0 to 10000
+
+for (let i=0; i<=10000; i++){
+    console.log(i);
+}
---
+++ step_39.js
@@ -0,0 +1,9 @@
+//print all numbers from 0 to 10000
+
+for (let i = 0; i <= 10000; i++){
+    let j=(i % 2);
+    if (j===0){
+        console.log(i);
+    }
+    else{
+    }}
